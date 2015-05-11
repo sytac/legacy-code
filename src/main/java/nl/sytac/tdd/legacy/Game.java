42ccbbd00580d4d4e5bbac060034d9081389909a
Michalik, AL (ITCDCC) - KLM <Adam.Michalik@klm.com>
2015-05-11 10:57:25 +0200

    Fix typo

--- src/main/java/nl/sytac/tdd/legacy/Game.java
+++ src/main/java/nl/sytac/tdd/legacy/Game.java
@@ -163,7 +163,7 @@ public class Game {
 
         } else {
 
-            System.out.println("Answer was corrent!!!!");
+            System.out.println("Answer was correct!!!!");
             purses[currentPlayer]++;
             System.out.println(players.get(currentPlayer)
                     + " now has "
